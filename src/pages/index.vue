64b35f93c9a093f7ba994c9000297f7040fb5aef
Arunstop <arunstop@gmail.com>
2023-01-22 12:07:45 +0700

    Update 11 files (#1)

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-container>
+    <v-row>
+      <template v-for="n in 4" :key="n">
+        <v-col class="mt-2" cols="12">
+          <strong>Category {{ n }}</strong>
+        </v-col>
+
+        <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+          <v-sheet height="150">
+            <v-btn color="primary"> Button </v-btn>
+          </v-sheet>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
