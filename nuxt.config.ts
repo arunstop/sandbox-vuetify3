a57675e7128f8ae775ba3e0b1ca7038973a686be
Arunstop <arunstop@gmail.com>
2023-01-22 19:39:09 +0000

    chore : added google fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,12 @@ export default defineNuxtConfig({
   srcDir: "src/",
   css: ["vuetify/lib/styles/main.sass", "@/assets/css/main.scss"],
   build: {
-    transpile: ["vuetify",'nuxt-icon'],
+    transpile: ["vuetify"],
   },
   vite: {
     define: {
       "process.env.DEBUG": false,
     },
   },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxtjs/google-fonts"],
 });
